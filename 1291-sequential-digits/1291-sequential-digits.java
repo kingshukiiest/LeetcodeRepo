ec21ec67de022345775da9b6b14c07068548b296
kingshukiiest <kingshukiiest@gmail.com>
2026-07-13 10:21:15 +0530

    Time: 0 ms (100%), Space: 42.5 MB (22.13%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -13,20 +13,28 @@ class Solution {
         int len1=countDigits(low);
         int len2=countDigits(high);
         String s ="123456789";
-        for(int l= len1 ; l<=len2 ; l++){
+        for(int length= len1 ; length<=len2 ; length++){
 
-            int i=0;
-            int j= i+l -1;
+            // int i=0;
+            // int j= i+length -1;
 
-            while(j<s.length()){
-                int digit=Integer.parseInt(s.substring(i,j+1));
+            // while(j<s.length()){
+            //     int digit=Integer.parseInt(s.substring(i,j+1));
+            //     if(digit > high) break;
+
+            //     if(digit >= low) {
+            //         list.add(digit);
+            //     }
+            //     i++;
+            //     j++;
+            // }
+            for( int i=0; i<=(s.length()-length); i++){
+                int digit=Integer.parseInt(s.substring(i,i+length));
                 if(digit > high) break;
 
                 if(digit >= low) {
                     list.add(digit);
                 }
-                i++;
-                j++;
             }
         }
         return list;
