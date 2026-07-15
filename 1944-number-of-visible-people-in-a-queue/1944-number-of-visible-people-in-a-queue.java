5943e795cdb66235904a401d426ae74db1782af4
kingshukiiest <kingshukiiest@gmail.com>
2026-07-15 10:02:35 +0530

    Time: 54 ms (43.61%), Space: 116 MB (89.41%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -1,20 +1,27 @@
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
-        int n=heights.length;
-        int[] ans = new int[n];
-        
-        for( int i=0; i<n ; i++){
-            int prevMax=Integer.MIN_VALUE;
+        int n= heights.length;
+        Stack <Integer> st= new Stack<>();
+        int [] ans=new int [n];
+        ans[n-1]=0;
+        st.push(n-1);
+
+        for( int i= n-2; i>=0 ; i--){
             int count=0;
-            for( int j=i+1; j<n ; j++){
-                if(prevMax < heights[j]){
-                    prevMax=heights[j];
-                    count++;
-                }
-                if(heights[j]>= heights[i]) break;
+            while(!st.isEmpty() && heights[st.peek()] <= heights[i]){
+                st.pop();
+                count++;
             }
-            ans[i]=count;
+            if(st.size()==0){
+                ans[i]=count;
+            }
+            else{
+                ans[i]=count+1;
+            }
+            
+            st.push(i);
         }
         return ans;
+
     }
 }
