c774e3dc6464400d871f9e53295d2bd29993976f
kingshukiiest <kingshukiiest@gmail.com>
2026-07-26 14:56:31 +0530

    Time: 189 ms (5.08%), Space: 42.6 MB (17.51%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.java
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.java
@@ -10,42 +10,18 @@ class Solution {
     }
     public int largestInteger(int n, int s) {
         int store=-1;
-        if(n==1){
-            for(int num=0; num<=9; num++){
-                if(sum(num)==s){
-                    store=num;
-                }
-            }
-        }
-        else if (n==2){
-            for(int num=0; num<=99; num++){
-                if(sum(num)==s){
-                    store=num;
-                }
-            }
-        }
-        else if (n==3){
-            for(int num=0; num<=999; num++){
-                if(sum(num)==s){
-                    store=num;
-                }
-            }
-        }
-        else if (n==4){
-            for(int num=0; num<=9999; num++){
-                if(sum(num)==s){
-                    store=num;
-                }
-            }
-        }
-        else if (n==5){
-            for(int num=0; num<=99999; num++){
-                if(sum(num)==s){
-                    store=num;
-                }
-            }
-        }
+        int end=-1;
+        if(n==1)  end=9;
+        else if (n==2) end=99;
+        else if (n==3) end=999;
+        else if (n==4) end= 9999;
+        else if (n==5) end= 99999;
 
+        for(int num=0; num<=end; num++){
+            if(sum(num)==s){
+                store=num;
+            }
+        }
         return store;
         
     }
