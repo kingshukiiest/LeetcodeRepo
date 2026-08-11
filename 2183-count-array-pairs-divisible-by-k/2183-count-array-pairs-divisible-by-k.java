299e9c9560852c149582abea8a234462fe4b2a45
kingshukiiest <kingshukiiest@gmail.com>
2026-08-12 00:41:39 +0530

    Time: 37 ms (94.95%), Space: 75.9 MB (94.95%) - LeetHub

---
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -0,0 +1,42 @@
+class Solution {
+    public long nc(int n){
+        return (1L*n*(n-1))/2;
+    }
+    public int gcd( int a , int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    public long countPairs(int[] nums, int k) {
+        int n=nums.length;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        // for(int i=0; i<n ; i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1); 
+        // }
+        for(int i=0; i<n ; i++){
+            int x= gcd(nums[i],k);
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+
+        long count1=0, count2=0;
+
+        for(int x : map.keySet()){
+            for(int y: map.keySet()){
+                if(x==y){ // own case
+                    // suppose (10,2) present and k=2
+                    // 10 is not divisible by 4 , but 100 is 
+                    //x%k==0 ||
+                    if((1L*x*x)%k==0){
+                        count2+=nc(map.get(x));
+                    }
+                    continue;
+                }
+                if(((1L*x*y)%k)==0){
+                    count1 +=(1L*map.get(x)*map.get(y));
+                }  
+            }
+        }
+
+        long ans=(count1)/2+ count2;
+        return ans;
+    }
+}
