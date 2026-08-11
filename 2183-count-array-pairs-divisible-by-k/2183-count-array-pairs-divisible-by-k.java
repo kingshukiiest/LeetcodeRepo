682757fc6cbae19316b81b2158e6d845b36aabcf
kingshukiiest <kingshukiiest@gmail.com>
2026-08-12 00:52:51 +0530

    Time: 35 ms (97.16%), Space: 75.6 MB (97.79%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -19,24 +19,41 @@ class Solution {
 
         long count1=0, count2=0;
 
-        for(int x : map.keySet()){
-            for(int y: map.keySet()){
-                if(x==y){ // own case
-                    // suppose (10,2) present and k=2
-                    // 10 is not divisible by 4 , but 100 is 
-                    //x%k==0 ||
-                    if((1L*x*x)%k==0){
-                        count2+=nc(map.get(x));
-                    }
-                    continue;
-                }
+        // for(int x : map.keySet()){
+        //     for(int y: map.keySet()){
+        //         if(x==y){ // own case
+        //             // suppose (10,2) present and k=2
+        //             // 10 is not divisible by 4 , but 100 is 
+        //             //x%k==0 ||
+        //             if((1L*x*x)%k==0){
+        //                 count2+=nc(map.get(x));
+        //             }
+        //             continue;
+        //         }
+        //         if(((1L*x*y)%k)==0){
+        //             count1 +=(1L*map.get(x)*map.get(y));
+        //         }  
+        //     }
+        // }
+        List<Integer> list= new ArrayList<>(map.keySet());
+      
+        for(int i=0; i<list.size() ;i++){
+            int x=list.get(i);
+            for(int j=i+1; j<list.size() ; j++){
+                int y=list.get(j);
                 if(((1L*x*y)%k)==0){
                     count1 +=(1L*map.get(x)*map.get(y));
-                }  
+                } 
+            }
+        }
+        for(int elem : map.keySet()){
+            if((1L*elem*elem)%k==0){
+                count2+=nc(map.get(elem));
             }
         }
 
-        long ans=(count1)/2+ count2;
+        long ans=count1+ count2;
+        // long ans=(count1)/2+ count2;
         return ans;
     }
 }
