4fa5c2366e0be4a6b88e806dee05dc731101f8d7
kingshukiiest <kingshukiiest@gmail.com>
2026-08-13 23:07:10 +0530

    Time: 8 ms (74.15%), Space: 65.3 MB (58.55%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,4 +1,5 @@
 class Solution {
+    Boolean [][] dp;
     public boolean isPalindrome(String s){
         int i=0;
         int j=s.length()-1;
@@ -19,7 +20,15 @@ class Solution {
         }
         for(int end=i;end<n; end++){
             String curr=s.substring(i,end+1);
-            if(isPalindrome(curr)){
+            boolean flag=false;
+            if(dp[i][end]!=null) {
+                flag=dp[i][end];
+            }
+            else{
+                flag=isPalindrome(curr);
+                dp[i][end]=flag;
+            }
+            if(flag){
                 l.add(curr);
                 solve(s,end+1,list,l);
                 l.remove(l.size()-1);
@@ -28,6 +37,8 @@ class Solution {
         }
     }
     public List<List<String>> partition(String s) {
+        int n=s.length();
+        dp= new Boolean [n][n];
         List<List<String>> list= new ArrayList<>();
         solve(s,0,list,new ArrayList<>());
 
