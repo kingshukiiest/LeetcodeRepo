22e2fd47e69442b6bf6d1a8332f764e10ad84e0c
kingshukiiest <kingshukiiest@gmail.com>
2026-08-21 01:28:19 +0530

    Time: 2787 ms (5.19%), Space: 48.1 MB (5.23%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,10 +1,10 @@
 class Solution {
-    class Pair {
-        int node; 
+    class Pair{
+        int node;
         int price;
         Pair(int node, int price){
-            this.node= node;
-            this.price= price;
+            this.node=node;
+            this.price=price;
         }
     }
     class State{
@@ -12,72 +12,54 @@ class Solution {
         int price;
         int stops;
         State(int node, int price, int stops){
-            this.node= node;
-            this.price= price;
-            this.stops= stops;
+            this.node=node;
+            this.price=price;
+            this.stops=stops;
         }
     }
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+    public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
         List<List<Pair>> list= new ArrayList<>();
         for(int i=0; i<n ; i++){
             list.add(new ArrayList<>());
         }
-        for( int [] e : flights){
-            int u=e[0];
-            int v= e[1];
-            int price=e[2];
-            list.get(u).add(new Pair(v,price));
+        for(int []edge : edges){
+            list.get(edge[0]).add(new Pair(edge[1],edge[2]));
         }
+        PriorityQueue<State> pq = new PriorityQueue<>(
+            (a,b)->Integer.compare(b.stops,a.stops)
+        );
+        pq.add(new State(src,0,-1));
 
-        int [][] minPrice= new int [n][k+1];
-        for( int [] arr : minPrice){
-            Arrays.fill(arr,Integer.MAX_VALUE);
-        }
+        int [][]minPrice= new int[n][k+1];
+        for(int [] a : minPrice){
+            Arrays.fill(a,Integer.MAX_VALUE);  
+        }  
         minPrice[src][0]=0;
 
-        PriorityQueue<State> pq= new PriorityQueue<>((a,b)->
-                            Integer.compare(a.price, b.price));
-        
-        pq.add(new State(src,0,-1)); // node, price, stops
-
         while(pq.size()>0){
-            State curr= pq.poll();
-    
-            if(curr.node==dst) {
+            State curr=pq.poll();
+            int node= curr.node;
+            int price=curr.price;
+            int stops=curr.stops;
+
+            if(curr.stops+1 > k){
                 continue;
             }
-            // if(minPrice[curr.node][curr.stops] < curr.price){
-            //     continue;
-            //     //dijkstra special
-            // }
 
-            // if(curr.stops == k){
-            //     continue;
-            // }
+            for(Pair p: list.get(node)){
+                int newStop=stops+1;
 
-            if(curr.stops < k){
-                int newStop= curr.stops+1;
-
-                for( Pair p : list.get(curr.node)){
-                    int totalPrice= p.price+ curr.price;
-
-                    if( totalPrice < minPrice[p.node][newStop]){
-                        minPrice[p.node][newStop]=totalPrice;
-                        pq.add(new State(p.node,totalPrice,newStop));
-                    }
+                int totalPrice=price+p.price;
+                if(totalPrice<minPrice[p.node][newStop]){
+                    minPrice[p.node][newStop]=totalPrice;
+                    pq.add(new State(p.node,totalPrice,newStop));
                 }
             }
         }
-        for( int i=0; i<n ; i++){
-            for(int j=0; j<=k ; j++){
-                System.out.print(minPrice[i][j]+" ");
-            }
-            System.out.println();
-        }
-
+        
         int ans=Integer.MAX_VALUE;
-        for(int i=0; i<=k ; i++){
-            ans= Math.min(ans,minPrice[dst][i]);
+        for(int j=0; j <k+1; j++){
+            ans=Math.min(ans,minPrice[dst][j]);
         }
         return (ans==Integer.MAX_VALUE)? -1 : ans;
     }
