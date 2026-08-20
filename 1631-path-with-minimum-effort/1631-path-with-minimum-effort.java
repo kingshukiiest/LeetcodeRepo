971f9463a3e36e206052547cf201bd339c4c8f6e
kingshukiiest <kingshukiiest@gmail.com>
2026-08-20 20:06:12 +0530

    Time: 41 ms (63.88%), Space: 47.9 MB (42.67%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,11 +1,11 @@
 class Solution {
-    class CompareEffort implements Comparator<Triplet>{
+    public class EffortCompare implements Comparator<Triplet>{
         @Override
         public int compare(Triplet a, Triplet b){
             return a.effort-b.effort;
         }
     }
-    class Triplet{
+     class Triplet{
         int r;
         int c;
         int effort;
@@ -15,46 +15,47 @@ class Solution {
             this.effort=effort;
         }
     }
+    
     public int minimumEffortPath(int[][] heights) {
         int m=heights.length;
         int n=heights[0].length;
-        
-        PriorityQueue<Triplet> pq= new PriorityQueue<>(new CompareEffort());
+        PriorityQueue<Triplet> pq= new PriorityQueue<>(
+            (a,b)->Integer.compare(a.effort,b.effort));
+        // PriorityQueue<Triplet> pq= new PriorityQueue<>(new EffortCompare);
 
-        int[][] minEffort= new int[m][n];
-        for( int [] a : minEffort){
+        int [][] minEffort= new int[m][n];
+        for(int []a : minEffort){
             Arrays.fill(a,Integer.MAX_VALUE);
         }
         minEffort[0][0]=0;
         pq.add(new Triplet(0,0,0));
-
-        int [][] dist={{0,1},{0,-1},{1,0},{-1,0}};
+        int [][] dir={{0,1},{0,-1},{1,0},{-1,0}};
 
         while(pq.size()>0){
-            Triplet front=pq.poll();
-            int r=front.r;
-            int c=front.c;
-            int effort=front.effort;
+            Triplet top= pq.poll();
+            int r=top.r;
+            int c= top.c;
+            int effort= top.effort;
 
-            if(r==m-1 && c==n-1) return effort;
-            
-            //dSpecial
             if(effort >minEffort[r][c]) continue;
+            //special 
+            if(r==m-1 && c== n-1) return effort;
 
-            for(int []row : dist){
-                int nr=r+row[0];
-                int nc=c+row[1];
-                if(nr>=0 && nr<m && nc>=0 && nc<n){
+            for(int []a : dir){
+                int nr=a[0]+r;
+                int nc=a[1]+c;
 
+                if(nr>=0 && nc>=0 && nr<m && nc <n ){
                     int totalEffort=Math.max(effort, Math.abs(heights[nr][nc]-heights[r][c]));
-                    
-                    if(minEffort[nr][nc] > totalEffort){
+                    if(totalEffort < minEffort[nr][nc]){
                         minEffort[nr][nc]=totalEffort;
-                        pq.add(new Triplet(nr,nc,totalEffort));
-                    }  
+                        pq.add( new Triplet(nr,nc,totalEffort));
+                    }
                 }
-            }
-        }   
+            } 
+
+        }    
+
         return minEffort[m-1][n-1];
     }
 }
