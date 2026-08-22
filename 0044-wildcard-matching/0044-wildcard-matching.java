b5c6e800fba68220b0648ea756dbe3b53cd138c4
kingshukiiest <kingshukiiest@gmail.com>
2026-08-22 12:41:16 +0530

    Time: 52 ms (30.51%), Space: 48.2 MB (27.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,59 +1,36 @@
 class Solution {
-    Boolean [][]dp;
-
-    public boolean helper(String s, String p, int i, int j){
-        int m=s.length();
-        int n=p.length();
-
-        if(i==m && j==n) return true;
-        if(i==m){
-            while(j<n){
-                if(p.charAt(j)!='*'){
-                    return false;
-                }
-                j++;
-            }
-            return true;
-        }
-        if(j==n) return false;
-
-        if(dp[i][j]!=null) return dp[i][j];
-
-
-        if(s.substring(i).equals(p.substring(j))) return dp[i][j]=true;
-
-        if(( s.charAt(i)==p.charAt(j)) 
-            || p.charAt(j)=='?'  ){
-                    return dp[i][j]=helper(s,p,i+1, j+1);
-        }
-        else if(s.charAt(i)!=p.charAt(j) && p.charAt(j)!='?' && p.charAt(j)!='*') {
-           
-            return dp[i][j]=false;
-        }
-        else if(p.charAt(j)=='*'){
-
-            boolean choice1=helper(s,p,i,j+1);
-            boolean choice2=helper(s,p,i+1, j);
-
-            return dp[i][j]=(choice1||choice2);
-
-            // while(j<n && p.charAt(j)=='*') j++;
-
-            // while(i<=m){
-            //     boolean flag=helper(s,p,i,j);
-            //     if(flag==true) return dp[i][j]=true; // if i==m dp will be out of bound, so take dp[m+1][n+1];
-            
-            //     i++;
-            // }
-        
-        }
-        return false;
-
-    }
     public boolean isMatch(String s, String p) {
-        int m=s.length();
-        int n=p.length();
-        dp=new Boolean[m][n];
-        return helper(s, p,0, 0);
+        int m=s.length(); //j
+        int n=p.length(); //i
+        // i for p , moving down
+        // j for s , moving right 
+        // in my table
+
+        boolean [][]dp= new boolean [n+1][m+1];
+        dp[0][0]=true;
+        // pattern is empty, s is not empty, so false;
+        for(int j=1; j<=m; j++){
+            dp[0][j]=false; // 0 means pattern is empty, length 0
+        }
+        // string is empty, pattern has some thing
+        for(int i=1; i<=n; i++){
+            if(p.charAt(i-1)=='*'){
+                dp[i][0]=dp[i-1][0];
+            }
+            else dp[i][0]=false; // it is already false;
+        }
+
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=m; j++){
+                if(s.charAt(j-1)==p.charAt(i-1) || p.charAt(i-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p.charAt(i-1)=='*'){
+                    dp[i][j]=dp[i-1][j]| dp[i][j-1];
+                }
+                else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 }
