e8caa9ffb9e1d9c60e0420c50d6074fa045d0f9e
kingshukiiest <kingshukiiest@gmail.com>
2026-08-22 16:02:07 +0530

    Time: 14 ms (92.2%), Space: 42.6 MB (97.56%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,48 +1,22 @@
 class Solution {
-    int [][]dp;
-    // public int helper(String s , String t, int i, int j){
-    //     if(j==t.length()) return 1;
+    int []space;
 
-    //     if(i==s.length()) return 0;
-
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     // if the char are equal you can pick only
-    //     // or you can skip, irrespective of the charater
-
-    //     int skip=helper(s,t,i+1, j);
-
-    //     int pick=0;
-    //     if(s.charAt(i)==t.charAt(j)){
-    //         pick=helper(s,t,i+1,j+1);
-    //     }
-    //     return dp[i][j]=pick+skip;
-    // }
     public int numDistinct(String s, String t) {
         int m =s.length();
         int n =t.length();
-        dp= new int[m+1][n+1];
-        // dp= new int[m][n];
-
-        // for(int [] a : dp){
-        //     Arrays.fill(a,-1);
-        // }
-        // return helper(s, t, 0,0);
-
-        dp[0][0]=1;
-        for(int i=1; i<=m; i++){
-            dp[i][0]=1;
-        }
-        
+        space= new int[n+1];
+        space[0]=1;
         for(int i=1; i<=m ; i++){
-            for(int j=1; j<=n; j++){
-                // skip
-                dp[i][j]=dp[i-1][j];
+            
+             // if i go left to write, diagonal elem will be overridden
+             // so go from right to left
 
+            for(int j=n; j>=1; j--){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]+=dp[i-1][j-1];
+                    space[j]+=space[j-1];// adding the diagonal elem actually
                 }
             }
         }
-        return dp[m][n];
+        return space[n];
     }
 }
