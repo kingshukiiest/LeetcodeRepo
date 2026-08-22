fce8d0465186895f6a7e0518326fa9530900ac26
kingshukiiest <kingshukiiest@gmail.com>
2026-08-22 11:29:39 +0530

    Time: 886 ms (5.06%), Space: 71.1 MB (6.78%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -5,19 +5,19 @@ class Solution {
         int m=s.length();
         int n=p.length();
 
-        if(dp[i][j]!=null) return dp[i][j];
-
-        if(i==m && j==n) return dp[i][j]=true;
+        if(i==m && j==n) return true;
         if(i==m){
             while(j<n){
                 if(p.charAt(j)!='*'){
-                    return dp[i][j]=false;
+                    return false;
                 }
                 j++;
             }
-            return dp[i][j]=true;
+            return true;
         }
-        if(j==n) return dp[i][j]=false;
+        if(j==n) return false;
+
+        if(dp[i][j]!=null) return dp[i][j];
 
 
         if(s.substring(i).equals(p.substring(j))) return dp[i][j]=true;
@@ -31,13 +31,20 @@ class Solution {
             return dp[i][j]=false;
         }
         else if(p.charAt(j)=='*'){
-            while(j<n && p.charAt(j)=='*') j++;
 
-            while(i<=m){
-                boolean flag=helper(s,p,i,j);
-                if(flag==true) return dp[i][j]=true;
-                i++;
-            }
+            boolean choice1=helper(s,p,i,j+1);
+            boolean choice2=helper(s,p,i+1, j);
+
+            return dp[i][j]=(choice1||choice2);
+
+            // while(j<n && p.charAt(j)=='*') j++;
+
+            // while(i<=m){
+            //     boolean flag=helper(s,p,i,j);
+            //     if(flag==true) return dp[i][j]=true; // if i==m dp will be out of bound, so take dp[m+1][n+1];
+            
+            //     i++;
+            // }
         
         }
         return false;
@@ -46,7 +53,7 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int m=s.length();
         int n=p.length();
-        dp=new Boolean[m+1][n+1];
+        dp=new Boolean[m][n];
         return helper(s, p,0, 0);
     }
 }
