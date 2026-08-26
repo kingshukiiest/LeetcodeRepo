f9e1f5ab0bc9cfb17e7bbf766ca0dd253fb3a46b
kingshukiiest <kingshukiiest@gmail.com>
2026-08-26 15:18:22 +0530

    Time: 46 ms (75.09%), Space: 115.7 MB (57.93%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,32 +1,25 @@
 class Solution {
-    public class CompareArray implements Comparator<int[]>{
-        @Override
-        public int compare(int []a,int []b){
-            if(a[0]==b[0]) return Integer.compare(a[1],b[1]);
-            return Integer.compare(a[0],b[0]);
-        }
-    }
     public int eraseOverlapIntervals(int[][] intervals) {
-        int n=intervals.length;
-        // Arrays.sort(intervals, (a,b)->(a[0]==b[0])? a[1]-b[1] : a[0]-b[0]);
-        Arrays.sort(intervals,new CompareArray());
-        for(int []a : intervals){
-            System.out.println(a[0]+" "+a[1]);
+
+        // Sort by end time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int count = 0;
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            // Overlap
+            if (intervals[i][0] < end) {
+                count++;
+            }
+
+            // No overlap
+            else {
+                end = intervals[i][1];
+            }
         }
 
-      
-        int end=intervals[0][1];
-        int kept=1;
-
-        for(int i=1; i<n; i++){
-            if(intervals[i][0] >= end){
-                kept++;
-                end=intervals[i][1];
-            }
-            else{
-                end=Math.min(end, intervals[i][1]);
-            }
-        }
-        return n-kept;
+        return count;
     }
 }
