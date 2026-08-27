08cdd527300984033e2a697c6556fa2b091207d9
kingshukiiest <kingshukiiest@gmail.com>
2026-08-27 20:00:24 +0530

    Time: 3 ms (87.65%), Space: 48.1 MB (60.56%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -1,25 +1,56 @@
 class Solution {
+    // public int lub(int i, int j, int []ages, double limit){
+    //     int store=-1;
+    //     while(i<=j){
+    //         int mid=i+(j-i)/2;
+    //         if(ages[mid] <= limit) i=mid+1;
+    //         else {
+    //             store=mid;
+    //             j= mid-1;
+    //         }
+    //     }
+    //     return store;
+    // }
+    // public int upperBound(int i, int j, int []ages, int elem){
+    //     int store=-1;
+    //     while(i<=j){
+    //         int mid=i+(j-i)/2;
+    //         if(ages[mid] <= elem) {
+    //             store=mid;
+    //             i=mid+1;
+    //         }
+    //         else {
+    //             j=mid-1;
+    //         }
+    //     }
+    //     return store;
+    // }
     public int numFriendRequests(int[] ages) {
         int n=ages.length;
         int req=0;
 
-        Arrays.sort(ages);
-        
-        for(int i=n-1; i>=1; i--){
-            int j=i-1;
-            while(j>=0){
-                int x=ages[i];
-                int y=ages[j];
-                if(!(y<=(x*0.5 +7))){
-                    if(x==y) req= req+2;
-                    else req++;
+        int []count=new int [121];
+        for(int age : ages){
+            count[age]++;
+        }
+        for(int i=1; i<=120; i++){
+            if(count[i]==0) continue;
+
+            for(int j=1;j<=120; j++){
+                if(count[j]==0) continue;
+
+                if(j>i) break;
+
+                if(j<= (0.5*i+7)) continue;
+
+                req+=(count[j]*count[i]);
+
+                if(i==j){
+                    req= req-count[j];
                 }
-                else{
-                    break;
-                }
-                j--;
             }
         }
+        
         return req;
 
     }
