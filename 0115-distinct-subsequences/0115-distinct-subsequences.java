ed8b5df057d16b6797d56186f3e13c38a9af1669
kingshukiiest <kingshukiiest@gmail.com>
2026-09-06 15:22:48 +0530

    Time: 20 ms (53.25%), Space: 54.5 MB (27.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,33 +1,41 @@
 class Solution {
     int [][]dp;
-    public int solve(String s, String t, int i, int j){
-        if(j<0) return 1;
-        if(i<0) return 0;
+    // public int helper(String s , String t, int i, int j){
+    //     if(j==t.length()) return 1;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        //skip
-        int skip=solve(s,t,i-1,j);
-        //pick
-        int pick=0;
-        if(s.charAt(i)==t.charAt(j)){
-            pick=solve(s,t,i-1,j-1);
-        }
+    //     if(i==s.length()) return 0;
 
-        return dp[i][j]=skip+pick;
-    }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     // if the char are equal you can pick only
+    //     // or you can skip, irrespective of the charater
+
+    //     int skip=helper(s,t,i+1, j);
+
+    //     int pick=0;
+    //     if(s.charAt(i)==t.charAt(j)){
+    //         pick=helper(s,t,i+1,j+1);
+    //     }
+    //     return dp[i][j]=pick+skip;
+    // }
     public int numDistinct(String s, String t) {
-        int m=s.length();
-        int n=t.length();
-
+        int m =s.length();
+        int n =t.length();
         dp= new int[m+1][n+1];
-        for(int i=0; i<m+1;i++){
+        // dp= new int[m][n];
+
+        // for(int [] a : dp){
+        //     Arrays.fill(a,-1);
+        // }
+        // return helper(s, t, 0,0);
+
+        dp[0][0]=1;
+        for(int i=1; i<=m; i++){
             dp[i][0]=1;
         }
-        for(int j=1; j<n+1;j++){
-            dp[0][j]=0;
-        }
+        
         for(int i=1; i<=m ; i++){
-            for(int j=1; j<=n ; j++){
+            for(int j=1; j<=n; j++){
+                // skip
                 dp[i][j]=dp[i-1][j];
 
                 if(s.charAt(i-1)==t.charAt(j-1)){
@@ -36,6 +44,5 @@ class Solution {
             }
         }
         return dp[m][n];
-        
     }
 }
