2e187e5ec9802de4e9e2250e9af8b89433fc929d
kingshukiiest <kingshukiiest@gmail.com>
2026-09-17 19:01:18 +0530

    Time: 0 ms (100%), Space: 80.2 MB (57.59%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -4,17 +4,17 @@ class Solution {
         int low=0;
         int high= n-1;
         int store=-1;
-        while(low<=high){
+        while(low<high){
             int mid= low+(high-low)/2;
             
             if(arr[mid]<arr[mid+1]){
                 low=mid+1;
             }
             else{
-                store=mid;
-                high=mid-1;
+                
+                high=mid;
             }
         }
-        return store;
+        return low;
     }
 }
